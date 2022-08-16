bac40b2607072bc9204b177fa678064e2c71caac
skelegor8 <skelegor8@gmail.com>
2022-08-16 13:37:27 +0200

    feat:  task 2 completed

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,18 +1,47 @@
 let btn = document.querySelector('.collapsible__button');
 let collapseBlock = document.querySelector('.collapsible__content');
+let collapsibleActionVisible = document.querySelector('.collapsible__action--visible')
+let collapsibleActionHidden = document.querySelector('.collapsible__action--hidden')
+
+let show = false;
+
+collapsibleActionVisible.style.display = "none";
+collapseBlock.style.opacity = 0;
 
 let collapseAnimationKey = new KeyframeEffect(
   collapseBlock, [
-    { transform: 'translateY(0%)' }, 
-    { transform: 'translateY(100%)' }
+    { opacity: 0 },
+    { transform: 'translateY(0%)' },
+    { transform: 'translateY(100%)' },
+    { opacity: 1 }
   ],
-      { duration: 3000, fill: 'forwards' }
+      { duration: 300, fill: 'forwards' }
 );
+
 let collapseAnimation = new Animation(collapseAnimationKey, document.timeline);
 
-function animation(event) {
-  btn.addEventListener("click", animation);
-  collapseAnimation.play();
+collapseAnimation.reverse();
+
+function animationShow() {
+  collapsibleActionHidden.style.display = "none";
+  collapsibleActionVisible.style.display = "block";
+  collapseAnimation.reverse();
+  show = true;
+  collapseBlock.style.opacity = 1;
 }
 
-btn.addEventListener("click", animation);
+function animationClose() {
+  collapsibleActionHidden.style.display = "block";
+  collapsibleActionVisible.style.display = "none";
+  collapseAnimation.reverse();
+  show = false;
+  collapseBlock.style.opacity = 0;
+}
+
+btn.addEventListener("click", () => {
+  if (show == true) {
+    animationClose();
+  } else {
+    animationShow();
+  }
+});
